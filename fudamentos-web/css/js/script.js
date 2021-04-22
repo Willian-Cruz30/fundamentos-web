dcb3f370ed571e83993cc446a1b9b0cb3de2107c
Willian-Cruz30 <williandacruzaraujo99@gmail.com>
2021-04-22 14:27:51 -0300

    Sistema bootstrap

--- fudamentos-web/css/js/script.js
+++ fudamentos-web/css/js/script.js
@@ -10,7 +10,10 @@ por seletor: querySelector()
 let nome = window.document.getElementById('nome')
 let email = document.querySelector('input#email')
 let assusnto = document.querySelector('#assunto')
-
+let nomeOK = false
+let emailOK = false
+let assuntoOK = false
+let mapa = document.querySelector('#mapa')
 nome.style.width = "100%"
 email.style.width = "100%"
 
@@ -23,6 +26,7 @@ function validarNome(){
     }else{
         txt.innerHTML = 'Nome Válido'
         txt.style.color = 'green'
+        nomeOK = true
     }
     
 }
@@ -36,6 +40,36 @@ function validarEmail(){
     }else{
         txt.innerHTML = 'E-mail Válido'
         txt.style.color = 'green'
+        emailOK = true
     }
 }
 
+function validarAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if (asssunto.value.legth>=100){
+        txtAssunto.innerHTML = 'Texto excedeu o limite de caracteres digite no maximo 100 caracteres'
+        txtAssunto.style.Color ='red'
+        txtAssunto.style.display = 'block'
+    }else{
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+
+function enviar(){
+    if (nomeOK == true && emailOK == true && assuntoOK == true){
+        alert ('Formulário enviado com sucesso!')
+    } else{
+        alert('Preencha o formulário corretamente')
+    }
+}
+
+function mapaZomm(){
+    mapa.style.width = '800px'
+    mapa.style.height = '600px'
+}
+function mapaNormal() {
+    mapa.style.width = '400px'
+    mapa.style.height = '250px'
+}
